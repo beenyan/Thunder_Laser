0152eb2f526150c69524f6341f4a4504abff30a3
BeenYan <asdfghjkl42045@gmail.com>
2020-12-06 15:53:31 +0800

    none

--- main.js
+++ main.js
@@ -3,6 +3,33 @@
 const canvas = document.getElementById('canvas'); // 取得畫布
 const ctx = canvas.getContext('2d'); // 宣告2D畫布
 
+class Laser {
+    constructor(args) {
+        let def = {
+            background_color: '#FEC400',
+            w: 80,
+            h: 6
+        }
+        Object.assign(def, args);
+        Object.assign(this, def);
+    }
+    draw() {
+        ctx.save();
+        ctx.beginPath();
+
+        ctx.translate(this.x, this.y);
+        ctx.rotate(Math.PI / 180 * this.deg);
+        ctx.fillStyle = this.background_color;
+        ctx.fillRect(0, 0, this.w, this.h);
+
+        ctx.closePath();
+        ctx.restore();
+    }
+    update() {
+        this.x += this.vx;
+        this.y += this.vy;
+    }
+}
 class Player {
     constructor(args) {
         let def = {
@@ -37,7 +64,6 @@ class Player {
         ctx.closePath();
     }
 }
-
 class Gui {
     constructor(args) {
         let def = {
@@ -57,8 +83,9 @@ class Gui {
         ctx.strokeRect(this.border_left, this.border_left, ww - this.border_left * 2, wh - this.border_left * 2)
     }
 }
+
 let windows = window;
-let pow = windows.innerHeight / 360
+let pow = windows.innerHeight / 360;
 let ww = 480 * pow;
 let wh = 360 * pow;
 canvas.width = windows.innerWidth;
@@ -67,17 +94,24 @@ let deviation = (canvas.width - ww) / 2;
 ctx.translate(deviation, 0)
 let gui = new Gui();
 let player = new Player();
+let Lasers = [];
+let time = 0;
 function init() {
-
+    gui.draw();
+    ctx.globalCompositeOperation = 'source-atop';
 }
-
+init();
 function update() {
-
+    ++time;
+    if (time % 80 === 0) {
+    }
+    Lasers.forEach(e => e.update());
 }
 
 function draw() {
     gui.draw();
     player.draw();
+    Lasers.forEach(e => e.draw());
     requestAnimationFrame(draw);
 }
 canvas.addEventListener('mousemove', e => {
@@ -86,3 +120,6 @@ canvas.addEventListener('mousemove', e => {
     player.y = Math.min(Math.max(e.offsetY, limit_move), wh - limit_move);
 })
 requestAnimationFrame(draw);
+setInterval(update, 1000 / 60);
+
+function rand(min, max) { return Math.floor(Math.random() * (max - min + 1)) + min }; // 隨機整數，含最大值、最小值 
